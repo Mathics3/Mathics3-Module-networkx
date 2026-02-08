7f3528f2b354f2f9e8297ea57e3fcc3fc47c93ed
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2026-02-08 12:19:18 -0300

    update boxes_to -> to methods

--- pymathics/graph/boxes.py
+++ pymathics/graph/boxes.py
@@ -25,35 +25,35 @@ class GraphBox(BoxElementMixin):
         self.G = G
         self.options = options
 
-    def boxes_to_b64text(
-        self, elements: Tuple[BaseElement] = None, **options
+    def to_b64text(
+        self, **options
     ) -> Tuple[bytes, Tuple[int, int]]:
         """
         Produces a base64 png representation and a tuple with the size of the pillow image
         associated to the object.
         """
-        contents, size = self.boxes_to_png(elements, **options)
+        contents, size = self.to_png(**options)
         encoded = base64.b64encode(contents)
         encoded = b"data:image/png;base64," + encoded
         return encoded, size
 
-    def boxes_to_png(self, elements=None, **options) -> Tuple[bytes, Tuple[int, int]]:
+    def to_png(self, **options) -> Tuple[bytes, Tuple[int, int]]:
         """
         returns a tuple with the set of bytes with a png representation of the image
         and the scaled size.
         """
         return png_format_graph(self.G, **self.options), (800, 600)
 
-    def boxes_to_svg(self, elements=None, **options):
+    def to_svg(self, **options):
         return svg_format_graph(self.G, **self.options), (400, 300)
 
-    def boxes_to_tex(self, elements=None, **options) -> str:
+    def to_tex(self, **options) -> str:
         """
         Store the associated image as a png file and return
         a LaTeX command for including it.
         """
 
-        data, size = self.boxes_to_png(elements, **options)
+        data, size = self.to_png(elements, **options)
         res = 100  # pixels/cm
         width_str, height_str = (str(n / res).strip() for n in size)
         head = rf"\includegraphics[width={width_str}cm,height={height_str}cm]"
@@ -70,11 +70,11 @@ class GraphBox(BoxElementMixin):
 
         return head + "{" + format(path) + "}"
 
-    def boxes_to_text(self, elements=None, **options):
+    def to_text(self, **options):
         return "-Graph-"
 
-    def boxes_to_mathml(self, elements=None, **options):
-        encoded, size = self.boxes_to_b64text(elements, **options)
+    def to_mathml(self, **options):
+        encoded, size = self.to_b64text(**options)
         decoded = encoded.decode("utf8")
         # see https://tools.ietf.org/html/rfc2397
         return f'<mglyph src="{decoded}" width="50%" height="50%" />'
